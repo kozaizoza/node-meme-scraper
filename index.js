1928b5cd5534f550602d9a8c208d5204540f8bb9
Azra Spahić-Haderer <azbogsunca@gmail.com>
2023-04-28 17:45:23 +0200

    Add download and save a file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-import fs from 'fs';
+// import { promises as fs } from 'node:fs';
+import * as fs from 'node:fs';
+import fetch from 'node-fetch';
 
-fs.mkdirSync('./meme', { recursive: true });
+// fs.mkdirSync('./meme', { recursive: true });
+//
 
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
@@ -8,11 +11,26 @@ const response = await fetch(
 const data = await response.text();
 console.log(data);
 
-const dir = './meme';
-
-function downloadImage(url, filepath) {
-  return download.image({
-    url,
-    dest: filepath,
+//
+const downloadFile = async (url, path) => {
+  const res = await fetch(url);
+  const fileStream = fs.createWriteStream(path);
+  await new Promise((resolve, reject) => {
+    res.body.pipe(fileStream);
+    res.body.on('error', reject);
+    fileStream.on('finish', resolve);
   });
-}
+};
+
+await downloadFile(
+  'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg',
+  './meme/01.jpg',
+);
+//
+
+// const imageUrl =
+//   'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg?width=300';
+
+// fetch(imageUrl).then((res) =>
+//   res.body.pipe(fs.createWriteStream('./path/to/image.png')),
+// );
